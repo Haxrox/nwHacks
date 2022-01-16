bc253e7a175d9b474921827069429a3f94827df8
Sajjadi-Code <sina.allen1@gmail.com>
2022-01-15 16:44:59 -0800

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
-const Header = ({text}) => {
+import PropTypes from 'prop-types'
+
+const Header = ({text, user, tokens}) => {
 
     return (
         <div>
-            <p>{text}</p>
+            <p style={textStyles}>{text}</p>
+            <h3>Welcome, {user}!</h3>
+            <div>
+                <p>You have {tokens} tokens</p>
+            </div>
         </div>
     )
 }
 
 Header.defaultProps = {
     text: "This is default text",
+    user: "Default Name",
+    tokens: 69,
+}
+
+const textStyles = {
+    color: 'blue',
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+    user: PropTypes.string,
 }
 
 export default Header
