ee1bb73c0dfb76a3e394bd1e117797c771f2a88c
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-16 10:23:56 -0800

    handle cancel request

--- src/pages/WaitPage.js
+++ src/pages/WaitPage.js
@@ -3,16 +3,34 @@ import { Spinner } from 'react-bootstrap'
 import Header from '../components/Header'
 import { Button } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+import {Firestore, Listen, Unsubscribe, UpdateDocument} from '../firebase.js';
+import {getAuth} from 'firebase/auth'
+import { doc, deleteField } from 'firebase/firestore'
 
 const WaitPage = () => {
 
     const history = useHistory();
-    const location = history.location.data
-    console.log(location)
+    const location = history.location.data;
+    console.log("WaitPage" + location);
+
+    const auth = getAuth();
+    Listen(doc(Firestore, location, "Requesters"), "requestersUpdated", (data => {
+        const requestInfo = data[auth.currentUser.uid];
+        if (requestInfo && requestInfo.requester) {
+            history.push({
+                pathname: "/match",
+                data: requestInfo.requester
+            })
+        }
+    }))
+
     const handleCancel = () => {
         //add backend to delete/cancel current response from database
-        
-        history.push("/home")};        
+        UpdateDocument(location, "Requesters", {
+            [auth.currentUser.uid]: deleteField()
+        })
+        history.push("/home")};    
+
     return (
         <div>
             
