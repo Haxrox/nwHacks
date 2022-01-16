8a0fd148f11cef3100986812a263073526899a4b
Sajjadi-Code <sina.allen1@gmail.com>
2022-01-16 00:31:55 -0800

    moved mock data to seatRequestPage from App.js

--- src/pages/SeatRequestPage.js
+++ src/pages/SeatRequestPage.js
@@ -2,8 +2,31 @@ import Header from '../components/Header'
 import AvailableSeat from '../components/AvailableSeat'
 import PropTypes from 'prop-types'
 
-const SeatRequestPage = ({building, seats, availableSeats}) => {
+const SeatRequestPage = ({building, seats}) => {
     console.log(seats);
+    const availableSeats = [
+        {
+          responderMessage: "Hi my name is Joe Mama and I'm sitting behind you",
+          leaveTime: "4:00PM",
+          waitTime: "5 mins",
+          tokenCost: "1",
+          id: "1",
+        },
+        {
+          responderMessage: "Hi my name is Sina Allen and I'm sitting in front of you",
+          leaveTime: "44:00PM",
+          waitTime: "55 mins",
+          tokenCost: "2",
+          id: "2",
+        },
+        {
+          responderMessage: "Hi my name is Kerry Wang and I'm sitting beside you",
+          leaveTime: "444:00PM",
+          waitTime: "555 mins",
+          tokenCost: "3",
+          id: "3",
+        },
+      ]
     return (
         <div>
             <Header />
