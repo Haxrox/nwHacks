ce60fed2acbae304d46ee3d970e2d85be1b5540f
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-15 22:38:49 -0800

    removed firebase sections

--- src/App.js
+++ src/App.js
@@ -6,37 +6,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage"
 
 import {useState} from 'react'
-import {collection, getDocs, query, onSnapshot, getFirestore } from "firebase/firestore"
 import { Routes } from './Routes';
 
-function parseSpaces(snapshot, setBuilding, setSeatCount) {
-  snapshot.forEach((doc) => {
-    console.log(doc);
-    setBuilding(doc.id);
-    const seat = doc.data().Floors.reduce((previousValue, currentValue) => 
-      previousValue.Seats.filter(seat => !seat.Occupied).length + currentValue.Seats.filter(seat => !seat.Occupied).length
-    );
-    console.log(seat);
-    setSeatCount(seat);
-  });
-}
-
-
 function App() {
-  const [building, setBuilding] = useState('');
-  const [seatCount, setSeatCount] = useState('');
-
-  const firestore = getFirestore();
-  const spaceCollection = collection(firestore, "Spaces");
-  (async() => {
-    const snapshot = await getDocs(spaceCollection);
-    parseSpaces(snapshot, setBuilding, setSeatCount);
-  })();
-
-  onSnapshot(query(spaceCollection), (snapshot => {
-    parseSpaces(snapshot, setBuilding, setSeatCount);
-  }));
-
   return (
     <div className="App">
       <Routes/>
