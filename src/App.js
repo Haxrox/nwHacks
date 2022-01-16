5bd210f7299e767295bba0c102c2e364d1980d66
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-15 18:52:52 -0800

    added login button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js';
 import SeatRequestPage from './components/SeatRequestPage.js';
+import Cheese from './components/Cheese.js';
 
 function App() {
   return (
     <div className="App">
       <SeatRequestPage />      
+      <Cheese />
     </div>
   );
 }
