9e9ab2a0bb326c3210e8ec796081f52400f49298
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-01-15 17:51:17 -0800

    rename firestore to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = require("./config/firebaseConfig.json");
 
 // Initialize Firebase
-const firestore = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
 
 console.log("Initialized firebase");
-export default firestore;
+export default firebase;
